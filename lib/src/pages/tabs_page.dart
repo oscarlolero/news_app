848937cc808a070474018c83511e88a4127e2821
Oscar Montes <oscmcojc1@live.com.mx>
2021-01-20 13:42:16 -0600

    Navigation between two tabs with Provider

--- lib/src/pages/tabs_page.dart
+++ lib/src/pages/tabs_page.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class TabsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _Pages(),
-      bottomNavigationBar: _Navigation(),
+    return ChangeNotifierProvider(
+      create: (_) => new _NavigationModel(),
+      child: Scaffold(
+        body: _Pages(),
+        bottomNavigationBar: _Navigation(),
+      ),
     );
   }
 }
@@ -13,8 +17,12 @@ class TabsPage extends StatelessWidget {
 class _Navigation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    final navigationModel = Provider.of<_NavigationModel>(context);
+
     return BottomNavigationBar(
-      currentIndex: 1,
+      currentIndex: navigationModel.actualPage,
+      onTap: (i) => navigationModel.actualPage = i,
       items: [
         BottomNavigationBarItem(icon: Icon(Icons.person_outline), label: 'Para ti'),
         BottomNavigationBarItem(icon: Icon(Icons.public), label: 'Encabezados'),
@@ -26,7 +34,10 @@ class _Navigation extends StatelessWidget {
 class _Pages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    final navegationModel = Provider.of<_NavigationModel>(context);
     return PageView(
+      controller: navegationModel.pageController,
       physics: NeverScrollableScrollPhysics(),
       //efecto de arrastre al final
       // physics: BouncingScrollPhysics(),
@@ -40,4 +51,19 @@ class _Pages extends StatelessWidget {
       ],
     );
   }
+}
+
+class _NavigationModel with ChangeNotifier {
+  int _actualPage = 0;
+  PageController _pageController = new PageController(initialPage: 0);
+
+  int get actualPage => this._actualPage;
+
+  set actualPage(int value) {
+    this._actualPage = value;
+    this._pageController.animateToPage(value, duration: Duration(milliseconds: 250), curve: Curves.easeOut);
+    notifyListeners();
+  }
+
+  PageController get pageController => this._pageController;
 }
